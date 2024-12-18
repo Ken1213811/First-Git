b8ca5a96d0964415d909a12c96d628e9b3182517
Ken1213811 <2430358702qq@gmail.com>
2024-12-18 07:49:47 +0000

    show rink

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,13 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts'=> $post->getPaginateByLimit()]);
+        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+
+    public function show(Post $post)
+    {
+        return view('post.show')->with([    'post'=> $post]);
     }
 }
+
